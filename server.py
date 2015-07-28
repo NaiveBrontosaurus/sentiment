5a22c2933f721313dd18ac3b11644745707bd811
Medhir Bhargava <medhirbhargava@gmail.com>
2015-07-27 19:09:07 -0700

    use NLTK module to classify tweets

--- server.py
+++ server.py
@@ -4,16 +4,20 @@ from flask import request
 import nltk
 import pickle
 
-#import trained NLTK classifier
-# f = open('classifier.pickle')
-# classifier = pickle.load(f)
-# f.close()
+# import trained NLTK classifier + word features
+f = open('classifier.pickle')
+classifier = pickle.load(f)
+f.close()
+f = open('word_features.pickle')
+word_features = pickle.load(f)
+f.close()
 
 def extractFeatures(document):
   document_words = set(document)
   features = {}
   for word in word_features:
     features['contains(%s)' % word] = (word in document_words)
+  return features
 
 def determineSentiment(tweet):
   return 'Positive'
@@ -24,7 +28,7 @@ app = Flask(__name__)
 @app.route('/sentiment')
 def getSentiment():
   tweet = request.args['text']
-  return determineSentiment(tweet)
+  return classifier.classify(extract_features(tweet))
 
 if __name__ == '__main__':
   app.run()
